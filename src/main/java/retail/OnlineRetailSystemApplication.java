008f4ce923f99b3932a3b41ea6d09f0c9d05a020
xuangiap10 <xuangiap10@gmail.com>
2023-05-22 01:27:46 -0500

    t

---
+++ src/main/java/retail/OnlineRetailSystemApplication.java
@@ -0,0 +1,13 @@
+package retail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRetailSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineRetailSystemApplication.class, args);
+	}
+
+}
